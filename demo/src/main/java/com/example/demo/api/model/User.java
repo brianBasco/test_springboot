9b00b20596307cdc02d2c7aa99e64933e89461cd
brianBasco <33673873+brianBasco@users.noreply.github.com>
2024-08-14 15:12:06 +0000

    up User data rest

--- demo/src/main/java/com/example/demo/api/model/User.java
+++ demo/src/main/java/com/example/demo/api/model/User.java
@@ -1,18 +1,13 @@
 package com.example.demo.api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 
 @Entity
 public class User {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
+    private @Id
+    @GeneratedValue Long id;
+  
     private String nom;
 
     public User() {
---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (name) VALUES (1, 'SEB');
+INSERT INTO user (name) VALUES (2, 'Marion');
